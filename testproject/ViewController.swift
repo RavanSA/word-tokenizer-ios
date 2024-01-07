fa79101a41f90e60843a3d88b54afd5cd213883d
Revan Sadigli <revan.sadigli@santsg.com>
2024-01-07 18:16:48 +0300

    Initial Commit

---
+++ testproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testproject
+//
+//  Created by Revan Sadigli on 7.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
